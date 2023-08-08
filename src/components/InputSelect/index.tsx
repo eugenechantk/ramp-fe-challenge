ce06258a84777c89ee5693839d71c1700e867f8d
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-08-07 22:50:27 -0700

    fix: Bug 1

--- src/components/InputSelect/index.tsx
+++ src/components/InputSelect/index.tsx
@@ -13,10 +13,10 @@ export function InputSelect<TItem>({
   loadingLabel,
 }: InputSelectProps<TItem>) {
   const [selectedValue, setSelectedValue] = useState<TItem | null>(defaultValue ?? null)
-  const [dropdownPosition, setDropdownPosition] = useState<DropdownPosition>({
-    top: 0,
-    left: 0,
-  })
+  // const [dropdownPosition, setDropdownPosition] = useState<DropdownPosition>({
+  //   top: 0,
+  //   left: 0,
+  // })
 
   const onChange = useCallback<InputSelectOnChange<TItem>>(
     (selectedItem) => {
@@ -59,7 +59,7 @@ export function InputSelect<TItem>({
             <div
               className="RampInputSelect--input"
               onClick={(event) => {
-                setDropdownPosition(getDropdownPosition(event.target))
+                // setDropdownPosition(getDropdownPosition(event.target))
                 toggleProps.onClick(event)
               }}
             >
@@ -71,7 +71,6 @@ export function InputSelect<TItem>({
                 "RampInputSelect--dropdown-container-opened": isOpen,
               })}
               {...getMenuProps()}
-              style={{ top: dropdownPosition.top, left: dropdownPosition.left }}
             >
               {renderItems()}
             </div>
@@ -117,15 +116,15 @@ export function InputSelect<TItem>({
   )
 }
 
-const getDropdownPosition: GetDropdownPositionFn = (target) => {
-  if (target instanceof Element) {
-    const { top, left } = target.getBoundingClientRect()
-    const { scrollY } = window
-    return {
-      top: scrollY + top + 63,
-      left,
-    }
-  }
+// const getDropdownPosition: GetDropdownPositionFn = (target) => {
+//   if (target instanceof Element) {
+//     const { top, left } = target.getBoundingClientRect()
+//     const { scrollY } = window
+//     return {
+//       top: scrollY + top + 63,
+//       left,
+//     }
+//   }
 
-  return { top: 0, left: 0 }
-}
+//   return { top: 0, left: 0 }
+// }
